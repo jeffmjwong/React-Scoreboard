83d8749aa8932a3575e54b85b1e29310776e687b
Jeff Wong <jeffmjwong@gmail.com>
2018-09-29 17:17:23 +1000

    Extract counter to its own component

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const Header = () => {
-  return (
-    <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
-    </header>
-  );
-}
+const Header = () => (
+  <header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players: 1</span>
+  </header>
+);
+
+const Player = () => (
+  <div className="player">
+    <span className="player-name">Guil</span>
+
+    <Counter />
+  </div>
+);
+
+const Counter = () => (
+  <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <span className="counter-score">35</span>
+    <button className="counter-action increment"> + </button>
+  </div>
+);
 
 ReactDOM.render(
-  <Header />,
+  <Player />,
   document.getElementById('root'),
 );
